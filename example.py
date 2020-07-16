7d8777349fd35c62e8a0759dd4ad948abf93bb9b
Yasir Saeed <55569291+yasir-saeed@users.noreply.github.com>
2020-07-16 18:43:10 -0400

    bootstrap added
    added homepage, navbar

--- example.py
+++ example.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+db = SQLAlchemy(app)
 
 @app.route('/<name>/')
 def user(name):
     return render_template('index.html', content=name)
 
 
+
 @app.route('/')
 def home():
-	return 'Home Page'
+	return render_template('home.html')
 
 @app.route('/error/')
 def error():
