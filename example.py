207926f6ba6641f07a5458076176aa5913cfbcd9
Yasir Saeed <55569291+yasir-saeed@users.noreply.github.com>
2020-07-18 04:48:27 -0400

    footer added
    basic footer with description and liinks

--- example.py
+++ example.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import Form
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+
 
 @app.route('/<name>/')
 def user(name):
     return render_template('index.html', content=name)
 
+@app.route('/about/')
+def about():
+	return render_template('about.html')
 
 
 @app.route('/')
